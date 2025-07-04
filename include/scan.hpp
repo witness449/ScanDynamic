832f5acafd06265ecd51e115847fabe475e67663
Dmitrii <kesha-84@yandex.ru>
2025-07-04 17:37:43 +0000

    Добавление комментариев к ф-ции scan

--- include/scan.hpp
+++ include/scan.hpp
@@ -19,8 +19,8 @@ consteval bool checkType() {
 template <typename... Ts>
 std::expected<details::scan_result<Ts...>, details::scan_error> scan(std::string_view input, std::string_view format) {
 
+    // Проверка переданных типов
     bool check_types = false;
-
     check_types = []<std::size_t... Is>(std::index_sequence<Is...>) -> bool {
         return (... && ([] -> bool {
                     if constexpr (!checkType<Ts>()) {
@@ -30,41 +30,41 @@ std::expected<details::scan_result<Ts...>, details::scan_error> scan(std::string
                     };
                 }()));
     }(std::index_sequence_for<Ts...>{});
-
     if (!check_types) {
         return std::unexpected(std::move(details::scan_error{"Incorrect used types"}));
     }
 
+    // Парсинг исходных вью
     auto parsed_sources = details::parse_sources<Ts...>(input, format);
     if (!parsed_sources.has_value()) {
         return std::unexpected(std::move(parsed_sources.error()));
     }
     auto [parsed_format, parsed_input] = parsed_sources.value();
-
     if (parsed_format.size() != sizeof...(Ts)) {
         return std::unexpected(std::move(details::scan_error{"Types are not correspond placeholders"}));
     }
 
-    // remove reference используется чтобы избежать проблем при выводе типов в случае ссылочных типов
+    // Формирование tuple с результатами парсинга значений
+    //  remove reference используется чтобы избежать проблем при выводе типов в случае ссылочных типов
     auto values = [&]<std::size_t... Is>(std::index_sequence<Is...>) {
         return std::make_tuple(
             details::parse_value_with_format<std::remove_reference_t<Ts>>(parsed_input[Is], parsed_format[Is])...);
     }(std::index_sequence_for<Ts...>{});
 
-    // Заводим переменную, чтобы в нее отправить ошибки спецификаторов
+    // Проверка на наличие в tuple результатов ошибок
+    //  Заводим переменную, чтобы в нее отправить ошибки спецификаторов
     std::string parsed_format_error{};
-    auto lambda = [&parsed_format_error](auto &arg) {
+    auto get_errors = [&parsed_format_error](auto &arg) {
         if (!arg.has_value()) {
             parsed_format_error += arg.error().message + '\n';
         }
     };
-
-    std::apply([&](auto &...arg) { (lambda(arg), ...); }, values);
-
+    std::apply([&](auto &...arg) { (get_errors(arg), ...); }, values);
     if (parsed_format_error.size() > 0) {
         return std::unexpected(std::move(details::scan_error{parsed_format_error}));
     }
 
+    // Если проверка показало что в tuple результатов нет ошибок формируем результат функции
     auto result = [&]<std::size_t... Is>(std::index_sequence<Is...>) -> details::scan_result<Ts...> {
         return {std::make_tuple((std::move(std::get<Is>(values)).value())...)};
     }(std::index_sequence_for<Ts...>{});
